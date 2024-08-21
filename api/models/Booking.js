00ca01c31bd90816531797cc3f55e3cb45f8ae74
ayushkhandare <108357023+ayushkhandare@users.noreply.github.com>
2024-08-21 13:03:08 +0530

    Add files via upload

---
+++ api/models/Booking.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  place: {type:mongoose.Schema.Types.ObjectId, ref:'Place'},
+  title: {type:mongoose.Schema.Types.ObjectId, ref:'Place'},
+  user: {type:mongoose.Schema.Types.ObjectId},
+  checkIn: {type:Date},
+  checkOut: {type:Date},
+  name: {type:String},
+  phone: {type:String},
+  price: Number,
+});
+
+const BookingModel = mongoose.model('Booking', bookingSchema);
+
+module.exports = BookingModel;
